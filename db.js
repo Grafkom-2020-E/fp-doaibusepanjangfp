06e0266c19fe0f5e922898b1fc070c4d4e74bf79
Wahed <arvenaloverz@gmail.com>
2021-01-11 18:57:59 +0700

    fix data structure of firebase and store to variable

--- db.js
+++ db.js
@@ -1,25 +1,18 @@
 //init db
 const db = firebase.firestore();
+let coordinates = {};
 
 db.enablePersistence().catch( err => {
-  if (err.code === 'unimplemented'){
-    console.log('persistence is not available');
-  }
+    if (err.code === 'unimplemented'){
+        console.log('persistence is not available');
+    }
 })
 
-db.collection("koordinat").where("status", "==", true).get()
-    .then(function(querySnapshot) {
-        // let maxTime =0, next_x, next_y;
+db.collection("koordinat")
+    .onSnapshot(function(querySnapshot) {
+        // let maxTime = Number.MAX_SAFE_INTEGER, next_x, next_y;
         querySnapshot.forEach(function(doc) {
-            // if(doc.data().inserted_at > maxTime){
-            //     maxTime = doc.data().inserted_at;
-            //     next_x  = doc.data().x;
-            //     next_y  = doc.data().y;
-            // }
-            console.log(doc.data().x + ", " + doc.data().y + " : " + doc.data().inserted_at);
+            // console.log(doc.id  + ":" + doc.data());
+            coordinates[doc.id] = doc.data()
         });
-        // console.log(next_x + ", " + next_y + " : " + maxTime);
-        // querySnapshot.docChanges().forEach( change => {
-        //     console.log(change.doc.data().x + ", " +change.doc.data().y);
-        // });
     });
